36c24050ed4b6f1aa18a8a7170bb2b78a181ce45
RushabhaJain <rushabhajain44@gmail.com>
2023-11-23 11:37:06 +0530

    Add mongodb Issue model

---
+++ src/db/issue.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const issueSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    status: {
+        type: String,
+        enum: ['New', 'In Progress', 'Closed'],
+        default: 'New'
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        default: 'Low'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastUpdatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+export const Issue = mongoose.model('Issue', issueSchema);
