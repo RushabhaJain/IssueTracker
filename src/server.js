4174ef06e9297ad3fe8385b5a875b991d5359691
RushabhaJain <rushabhajain44@gmail.com>
2023-11-23 18:21:41 +0530

    Added Routes of issues and users to express app

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDB } from './db/index.js';
+import IssueRouter from './router/issue.js';
+import UserRouter from './router/user.js';
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// REST APIs
+app.use('/issues', IssueRouter);
+app.use('/users', UserRouter);
+
 
 const PORT = process.env.PORT || 3000;
 
