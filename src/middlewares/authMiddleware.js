6112030543af4eba33a7145d18ca5e4e3f4b6f39
RushabhaJain <rushabhajain44@gmail.com>
2023-11-28 18:18:56 +0530

    Formatted the file content

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,28 +2,28 @@ import { User } from "../models/user";
 import { getTokenContent } from "../utils/token";
 
 export const authMiddleware = async (req, res, next) => {
-    const token = req.headers.authorization || req.cookies.token;
-    if (!token) {
-        return res.status(401).send({
-            success: false,
-            error: "No token provided"
-        });
+  const token = req.headers.authorization || req.cookies.token;
+  if (!token) {
+    return res.status(401).send({
+      success: false,
+      error: "No token provided",
+    });
+  }
+  try {
+    const decoded = await getTokenContent(token);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).send({
+        success: false,
+        error: "No user found",
+      });
     }
-    try {
-        const decoded = await getTokenContent(token);
-        const user = await User.findById(decoded.id);
-        if (!user) {
-            return res.status(401).send({
-                success: false,
-                error: "No user found"
-            });
-        }
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(401).send({
-            success: false,
-            error: "Invalid token"
-        });
-    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).send({
+      success: false,
+      error: "Invalid token",
+    });
+  }
 };
