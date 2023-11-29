20679c18ce69658798b9db9feab95bad203550fd
RushabhaJain <rushabhajain44@gmail.com>
2023-11-29 09:00:37 +0530

    Removed un-necessary code

--- src/utils/token.js
+++ src/utils/token.js
@@ -6,12 +6,13 @@ const secret = process.env.JWT_SECRET;
 const expiration = process.env.JWT_EXPIRATION;
 
 export const generateAccessToken = (payload) => {
-    return jwt.sign(payload, secret, { expiresIn: expiration, httpOnly: true });
+    const token = jwt.sign(payload, secret, { expiresIn: expiration });
+    return token;
 };
 
-export const getTokenContent = async (token) => {
+export const getTokenContent = (token) => {
     try {
-        return await jwt.verify(token, secret);
+        return jwt.verify(token, secret);
     } catch (err) {
         throw new Error('Invalid token');
     }
