5d5ffa6e20868387fcd04329c8884364dbbd5997
RushabhaJain <rushabhajain44@gmail.com>
2023-11-29 14:53:08 +0530

    Refactored middlewares to handle bearer token and created new middleware that will validate the user but incase of invalidation will not send the response

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,15 @@ import { User } from "../models/user.js";
 import { getTokenContent } from "../utils/token.js";
 
 export const authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization || req.cookies.token;
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else {
+    token = req.cookies.token;
+  }
   if (!token) {
     return res.status(401).send({
       success: false,
@@ -25,5 +33,30 @@ export const authMiddleware = async (req, res, next) => {
       success: false,
       error: "Invalid token",
     });
+  }
+};
+
+export const authGraphQLMiddleware = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else {
+    token = req.cookies.token;
+  }
+  try {
+    if (token) {
+      const decoded = await getTokenContent(token);
+      const user = await User.findById(decoded.id);
+      if (user) {
+        req.user = user;
+      }
+    }
+  } catch (error) {
+    console.log("[Auth Middleware GraphQL] Error: ", error);
+  } finally {
+    next();
   }
 };
