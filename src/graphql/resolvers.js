e06f5c126603f7ba089d14d524b5cf39c87dedb3
RushabhaJain <rushabhajain44@gmail.com>
2023-11-29 17:29:14 +0530

    Added unauthorized error message

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,15 +1,25 @@
+import { GraphQLError } from "graphql";
 import { getIssuesCreatedByUser, getIssueById, createIssue, updateIssue, deleteIssue } from "../services/issue.js";
 import { getUserById } from "../services/user.js";
 
 export const resolvers = {
     Query: {
         issues: async (_, params, { user }) => {
+            if (!user) {
+                throw unauthorizedError("You must be logged in to view issues");
+            }
             return await getIssuesCreatedByUser(user._id);
         },
-        issue: async (_, { id }) => {
+        issue: async (_, { id }, { user }) => {
+            if (!user) {
+                throw unauthorizedError("You must be logged in to view issues");
+            }
             return await getIssueById(id);
         },
         user: async (_, params, { user }) => {
+            if (!user) {
+                throw unauthorizedError("You must be logged in to view issues");
+            }
             return await getUserById(user._id);
         }
     },
@@ -23,17 +33,35 @@ export const resolvers = {
     },
     Mutation: {
         createIssue: async (_, { input }, { user }) => {
+            if (!user) {
+                throw unauthorizedError("You must be logged in to view issues");
+            }
             const issue = {
                 ...input,
                 createdBy: user._id
             };
             return await createIssue(issue);
         },
-        updateIssue: async (_, { id, input }) => {
+        updateIssue: async (_, { id, input }, { user }) => {
+            if (!user) {
+                throw unauthorizedError("You must be logged in to view issues");
+            }
             return await updateIssue(id, input);
         },
-        deleteIssue: async (_, { id }) => {
+        deleteIssue: async (_, { id }, { user }) => {
+            if (!user) {
+                throw unauthorizedError("You must be logged in to view issues");
+            }
             return await deleteIssue(id);
         }
     }
+};
+
+const unauthorizedError = (msg) => {
+    return new GraphQLError(msg, {
+        statusCode: 401,
+        extensions: {
+            code: "UNAUTHORIZED"
+        }
+    });
 };
